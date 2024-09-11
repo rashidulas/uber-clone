882b3c9110cbd1fc41e144430bbff899fc70a95f
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2024-09-11 11:30:02 -0500

    Adding signin signup onboarding

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+        <h1>Home</h1>
+        <p>Welcome to the home page</p>
+        </SafeAreaView>
+    );
+}
+
+export default Home;
